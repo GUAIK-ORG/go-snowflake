0cfc2e6799836b33d47152a68899fef03532bd0a
guaik <luting.gu@gmail.com>
2020-01-12 14:38:30 +0800

    添加README.md

---
+++ README.md
@@ -0,0 +1,27 @@
+# 🌀GO-Snowflake
+
+## Snowflake简介
+
+在单机系统中我们会使用自增id作为数据的唯一id，自增id在数据库中有利于排序和索引，但是在分布式系统中如果还是利用数据库的自增id会引起冲突，自增id非常容易被爬虫爬取数据。在分布式系统中有使用uuid作为数据唯一id的，但是uuid是一串随机字符串，所以它无法被排序。
+
+Twitter设计了Snowflake算法为分布式系统生成ID,Snowflake的id是int64类型，它通过datacenterId和workerId来标识分布式系统，下面看下它的组成：
+
+| 1bit | 41bit | 5bit | 5bit | 12bit |
+|---|---|---|---|---|
+| 符号位（保留字段） | 时间戳(当前时间-纪元时间) | 数据中心id | 机器id | 自增序列
+
+### 算法简介
+
+在使用Snowflake生成id时，首先会计算时间戳timestamp（当前时间 - 纪元时间），如果timestamp数据超过41bit则异常。同样需要判断datacenterId和workerId不能超过5bit(0-31)，在处理自增序列时，如果发现自增序列超过12bit时需要等待，因为当前毫秒下12bit的自增序列被用尽，需要进入下一毫秒后自增序列继续从0开始递增。
+
+---
+
+## 快速开始
+
+### 安装
+
+`git clone https://github.com/GUAIK-ORG/go-snowflake.git`
+
+### 运行
+
+`go run main.go`
--- snowflake/snowflake.go
+++ snowflake/snowflake.go
@@ -11,15 +11,15 @@ import (
 const (
 	epoch             = int64(1577808000000)                           // 设置起始时间：2020-01-01 00:00:00，有效期69年
 	timestampBits     = uint(41)                                       // 纪元占用位数
-	datacenteridBits  = uint(5)                                        // 数据标示id所占位数
+	datacenteridBits  = uint(5)                                        // 数据中心id所占位数
 	workeridBits      = uint(5)                                        // 机器id所占位数
 	sequenceBits      = uint(12)                                       // 序列所占的位数
 	timestampMax      = int64(-1 ^ (-1 << timestampBits))              // 时间戳最大值
-	datacenteridMax   = int64(-1 ^ (-1 << datacenteridBits))           // 支持的最大数据标示id数量
+	datacenteridMax   = int64(-1 ^ (-1 << datacenteridBits))           // 支持的最大数据中心id数量
 	workeridMax       = int64(-1 ^ (-1 << workeridBits))               // 支持的最大机器id数量
 	sequenceMask      = int64(-1 ^ (-1 << sequenceBits))               // 支持的最大序列id数量
 	workeridShift     = sequenceBits                                   // 机器id左移位数
-	datacenteridShift = sequenceBits + workeridBits                    // 数据标示左移位数
+	datacenteridShift = sequenceBits + workeridBits                    // 数据中心id左移位数
 	timestampShift    = sequenceBits + workeridBits + datacenteridBits // 时间戳左移位数
 )
 
